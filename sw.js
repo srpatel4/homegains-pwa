a1009c91be9dbb8bb55451e7252093d0f3d4b1ca
srpatel4 <ygygygygygyg232@gmail.com>
2026-05-05 20:09:53 -0400

    Initial commit: sw.js

---
+++ sw.js
@@ -0,0 +1,49 @@
+const CACHE_NAME = 'homegains-v2';
+const ASSETS = [
+  './',
+  './index.html',
+  './styles.css',
+  './script.js',
+  './landing.html',
+  './landing.css',
+  './landing.js',
+  './manifest.json',
+  './icon.svg'
+];
+
+self.addEventListener('install', (e) => {
+  self.skipWaiting();
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      );
+    }).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    fetch(e.request)
+      .then((response) => {
+        const responseClone = response.clone();
+        caches.open(CACHE_NAME).then((cache) => {
+          cache.put(e.request, responseClone);
+        });
+        return response;
+      })
+      .catch(() => {
+        return caches.match(e.request);
+      })
+  );
+});
